a6cb3e11484be2b15bf19c0df4a44f8b60aa33b9
Clément <clement.delord@epitech.eu>
2022-03-14 15:20:11 +0100

    ex 1 - 4

--- Ex/Ex1.md
+++ Ex/Ex1.md
@@ -5,3 +5,5 @@
 2. Associer cette méthode à la route /chance/nombre
 
 3. Créer une page qui affiche un jour aléatoire de la semaine (Lundi, Mardi, etc...), créer la route et la méthode.
+
+4. Créer une page d'accueil (/) qui affiche un message de bienvenue
--- src/Controller/ChanceController.php
+++ src/Controller/ChanceController.php
@@ -2,20 +2,20 @@
 
 namespace App\Controller;
 
-//ic ion importe les composants qu'on utilise dans le controller
+//ici on importe les composants qu'on utilise dans le controller
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ChanceController
 {
 
-    //On utilise annotation plutôt que le système de routing classque (config/routes.yaml)
+    //On utilise annotation plutôt que le système de routing classique (config/routes.yaml)
     /**
-     * @Route("/chance/nombre")
+     * @Route("/chance/nombre/{min}/{max}")
      */
-    public function nombre(): Response //On précise le type de donnée retournée pour une meilleure compréhension du debug
+    public function nombre(int $min = 0, int $max = 100): Response //On précise le type de donnée retournée pour une meilleure compréhension du debug
     {
-        $nombre = random_int(0, 100);
+        $nombre = random_int($min, $max);
 
         //Une méthode de controller DOIT renvoyer un objet de type Response
         return new Response(
