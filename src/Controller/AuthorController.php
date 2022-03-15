4853217b94dc5042e6271ef96849885a81f82fdd
Clément <clement.delord@epitech.eu>
2022-03-15 12:05:31 +0100

    doctrine

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorController extends AbstractController
+{
+    /**
+     * @Route('author/add')
+     */
+    public function add () :Response
+    {
+        $author = new Author();
+
+        $form = $this->createFormBuilder($author)
+            ->add('name', TextType::class)
+            ->getForm();
+
+        return $this->render('author/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
