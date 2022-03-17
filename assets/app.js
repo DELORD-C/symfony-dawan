5599a2ce72d6a79385919cfd66d9212c76905109
Clément <clement.delord@epitech.eu>
2022-03-17 09:58:26 +0100

    .env.sample & assets copying & linking

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.css';
+import './styles/style.css';
+
+import "bootstrap/scss/bootstrap.scss";
+
+//Importation des medias un par un
+// import image from './media/img/test.jpeg';
+
+require('bootstrap');
+
+// start the Stimulus application
+import './bootstrap';
